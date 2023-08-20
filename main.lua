8234b8f36a29a8db48da5fe3aef3c654d2b7c37e
1vnazarov <nazaroffvana.boeing@yandex.ru>
2023-08-20 11:07:51 +0300

    Все переписано

--- main.lua
+++ main.lua
@@ -3,15 +3,6 @@ sh = display.contentHeight
 
 local box = native.newTextField(sw / 2, sh / 2, sw, sh / 5)
 
---[[function string:split(sep, reg)
-    local res = {}
-    reg = reg and sep or string.format("([^%s]+)", sep)
-    for v in self:gmatch(reg) do
-        table.insert(res, v)
-    end
-    return res
-end]]
-
 function string:replace(old, new)
     local b, e = self:find(old, 1, true)
     if not b then
@@ -19,6 +10,33 @@ function string:replace(old, new)
     else
         return self:sub(1, b - 1) .. new .. self:sub(e + 1)
     end
+end
+
+function string:split(sep, plain)
+    local b, res = 0, {}
+    sep = sep or "%s+"
+
+    if #sep == 0 then
+        for i = 1, #self do
+            res[#res + 1] = self:sub(i, i)
+        end
+        return res
+    end
+
+    while b <= #self do
+        local e, e2 = self:find(sep, b, plain)
+        if e then
+            res[#res + 1] = self:sub(b, e - 1)
+            b = e2 + 1
+            if b > #self then
+                res[#res + 1] = ""
+            end
+        else
+            res[#res + 1] = self:sub(b)
+            break
+        end
+    end
+    return res
 end
 
 function table.contains(tbl, value)
@@ -30,13 +48,9 @@ function table.contains(tbl, value)
     return false
 end
 
---[[function table.pop(tbl)
-    return table.remove(tbl, 1)
-end]]
-
 local m = require("parser")
 box:addEventListener("userInput", function(event)
     if event.phase == "submitted" then
-        print(m.calculate(event.target.text))
+        print("RES:", m.calculate(event.target.text))
     end
 end)
--- parser.lua
+++ parser.lua
@@ -1,77 +1,171 @@
 local m = {}
 
 function m.calculate(expr)
-    local tokens = {"+", "-", "*", "/", "^", "%", "!"}
-
-    local function factorial(n)
-        if n == 0 then
-            return 1
-        end
-        return n * factorial(n - 1)
+    expr = tostring(expr):gsub("%s+", "")
+    if tonumber(expr) then
+        return tonumber(expr)
     end
+    local tokens = {"!", "^", "*", "/", "%", "+", "-"}
+    local consts = {
+        pi = math.pi,
+        e = math.exp(1)
+    }
 
-    local function calc(left, operator, right)
-        if operator == "!" then
-            return factorial(left)
-        elseif operator == "^" then
-            return left ^ right
-        elseif operator == "*" then
-            return left * right
-        elseif operator == "/" then
-            return left / right
-        elseif operator == "%" then
-            return left % right
-        elseif operator == "+" then
-            return left + right
-        elseif operator == "-" then
-            return left - right
-        end
-    end
-
-    local function parse(expr)
-        local left, right = "", ""
-        local operator
-        local i = 0
-
-        for s in expr:gmatch(".") do
-            i = i + 1
-
-            if table.contains(tokens, s) then
-                if operator then
-                    print(expr, left, operator, right, calc(left, operator, right), expr:sub(i))
-                    -- 8^2+(7*5+(2*2))
-                    expr = calc(left, operator, right) .. expr:sub(i)
-                    return parse(expr)
+    local function replaceConstants(expr, vars) -- На самом деле, не только замена констант, но еще и переменных
+        for name, value in pairs(vars or consts) do
+            -- Проверяем, что перед и после имени константы нет букв
+            local pattern = ("([^%a%d%.])"):rep(#name) .. name .. ("([^%a%d%.])"):rep(#name)
+            expr = expr:gsub(pattern, function(left, right)
+                local left_bracket, right_bracket = left:sub(-1), right:sub(1)
+                if (left_bracket == "(" or left_bracket == "=") and right_bracket == ")" then
+                    -- В найденном совпадении заменяем имя константы на ее значение
+                    return left .. tostring(value) .. right
+                else -- Отменяем замену, если вокруг имени есть скобки
+                    return left .. name .. right
                 end
-                operator = s
+            end)
+    
+            -- Обработка первого символа в выражении, если он является константой
+            if expr:sub(1, #name) == name then
+                expr = tostring(value) .. expr:sub(#name + 1)
             end
-            
-            if tonumber(s) or s == "." then
-                if not operator then
-                    left = left .. s
-                else
-                    right = right .. s
-                end
+    
+            -- Обработка последнего символа в выражении, если он является константой
+            if expr:sub(-#name) == name then
+                expr = expr:sub(1, #expr - #name) .. tostring(value)
             end
-        end
-        return left, operator, right
-    end
-
-    local function brackets(expr)
-        local sub = ""
-        for s in expr:gmatch(".") do
-            if s == "(" then
-                sub = ""
-            elseif s == ")" then
-                return brackets(expr:replace("(" .. sub .. ")", m.calculate(sub)))
-            else
-                sub = sub .. s
-            end
+    
+            -- Обработка вхождений констант в аргументах функций
+            expr = expr:gsub("%b()", function(arg)
+                local replaced = replaceConstants(arg:sub(2, #arg - 1)) -- Заменяем константы внутри скобок
+                return "(" .. replaced .. ")" -- Возвращаем скобки вокруг замененного значения
+            end)
         end
         return expr
     end
-    expr = brackets(expr)
-    print(expr)
-    return calc(parse(expr))
+
+    local funcs = {
+        sin = math.sin,
+        cos = math.cos,
+        tan = math.tan,
+        ctg = function(x)
+            return math.cos(x) / math.sin(x)
+        end,
+        abs = math.abs,
+        deg = math.deg,
+        integral = function(func, a, b, n)
+            n = n or 100
+            local h = (b - a) / n
+            local k = 0
+            local x = a + h
+            local function f(x)
+                return m.calculate(replaceConstants(func, {x = x}))
+            end
+            for _ = 1, n / 2 do
+                k = k + 4 * f(x)
+                x = x + 2 * h
+            end
+            x = a + 2 * h
+            for _ = 1, n / 2 - 1 do
+                k = k + 2 * f(x)
+                x = x + 2 * h
+            end
+            return (h / 3) * (f(a) + f(b) + k)
+        end
+    }
+
+    local function factorial(n)
+        if n < 0 or math.floor(n) ~= n then
+            return
+        end
+        local res = 1
+        for i = 2, n do
+            res = res * i
+        end
+        return res
+    end
+    
+    local function calc(left, operator, right)
+        if operator ~= "!" and (right == nil or left == nil) then
+            return
+        end
+
+        local operators = {
+            ["!"] = function() return factorial(left) end,
+            ["^"] = function() return left ^ right end,
+            ["*"] = function() return left * right end,
+            ["/"] = function() return left / right end,
+            ["%"] = function() return left % right end,
+            ["+"] = function() return left + right end,
+            ["-"] = function() return left - right end
+        }
+        return operators[operator]()
+    end
+
+    local function getLeftOperand(expr, index, stopAtExclamation)
+        local left = ""
+        for i = index - 1, 1, -1 do
+            local s = expr:sub(i, i)
+            if string.find("+-*/^%" .. (stopAtExclamation and "!" or "") .. "()", s, 1, true) then
+                break
+            end
+            left = s .. left
+        end
+        return left:match("^[%w.]+") or ""
+    end
+
+    local function parse(expr)
+        expr = expr:gsub("%b()", function(subexpr)
+            return m.calculate(subexpr:sub(2, -2))
+        end)
+
+        for _, op in ipairs(tokens) do
+            local index = 1
+            while index <= #expr do
+                local s = expr:sub(index, index)
+
+                if s == op then
+                    local left = getLeftOperand(expr, index, op == "!")
+                    local right = op ~= "!" and expr:sub(index + 1):match("^[%w.]+") or nil
+                    local result = calc(tonumber(left), s, tonumber(right))
+                    if not result then
+                        return
+                    end
+                    expr = expr:sub(1, index - #left - 1) .. tostring(result) .. expr:sub(index + #left + (op ~= "!" and #right or 0))
+
+                    index = 1
+                else
+                    index = index + 1
+                end
+            end
+        end
+        return tonumber(expr)
+    end
+
+    local function calcFunctions(expr)
+        local ignore = {
+            integral = {1}
+        }
+        return expr:gsub("([^%(]+)%((.+)%)", function(func, args)
+            print(func, args)
+            local argsTbl = {}
+            local i = 0
+            for arg in args:gmatch("([^,]+)") do
+                i = i + 1
+                table.insert(argsTbl, (ignore[func] and table.contains(ignore[func], i)) and arg or m.calculate(arg))
+            end
+
+            if funcs[func] then
+                return funcs[func](unpack(argsTbl))
+            else
+                return func .. "(" .. args .. ")"
+            end
+        end)
+    end
+    
+    expr = replaceConstants(expr)
+    expr = calcFunctions(expr)
+    return parse(expr) or expr
 end
+
 return m
